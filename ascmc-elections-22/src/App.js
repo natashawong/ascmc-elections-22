4113c6d599656e1e5feae4429e64335114dbf709
natashawong <natasha.wong10@gmail.com>
2022-02-25 20:54:59 -0800

    update spacing

--- ascmc-elections-22/src/App.js
+++ ascmc-elections-22/src/App.js
@@ -16,40 +16,71 @@ function App() {
             
           <p>The deadline to turn in signatures and become a declared candidate is Sunday 2/27 at 11:59pm. Speeches will be Monday 3/1 at snack. Contact clay22@cmc.edu with any questions</p>
           <h4 style={{color: 'red'}}><b>This website will be updated regularly with all candidates that will appear on the ballot as more join the race</b></h4>
-          <h3>President</h3>
-          <ul>
-            <li>Josh Nagra</li>
-            <li>Jon Joey Telebrico</li>
-            <li>Colin Sam</li>
-          </ul>
-          <h3>Executive Vice President</h3>
-          <ul>
-            <li>Zane Yamamoto</li>
-          </ul>
-          <h3>Vice President of Student Activities</h3>
-          <ul>
-            <li>Christopher LaRovere</li>
-          </ul>
-          <h3>Dormitory Affairs Chair</h3>
-          <ul>
-            <li>Kirby Kimball</li>
-          </ul>
-          <h3>Senior Class President (2023)</h3>
-          <ul>
-            <li>Sam Bogen</li>
-            <li>Sophie Dvorkin</li>
-            <li>Amari Huang</li>
-          </ul>
-          <h3>Junior Class President (2023)</h3>
-          <ul>
-            <li>Cristian Pang</li>
-            <li>Ian Blaime</li>
-          </ul>
-          <h3>Sophomore Class President (2023)</h3>
-          <ul>
-            <li>Giovanni Pierre</li>
-            <li>Bryan Soh</li>
-          </ul>
+
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+              <h3>President</h3>
+              <ul>
+                <li>Josh Nagra</li>
+                <li>Jon Joey Telebrico</li>
+                <li>Colin Sam</li>
+              </ul>
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+              <h3>Executive Vice President</h3>
+              <ul>
+                <li>Zane Yamamoto</li>
+              </ul>
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+              <h3>Vice President of Student Activities</h3>
+              <ul>
+                <li>Christopher LaRovere</li>
+              </ul>
+            </div>
+          </div>
+
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+            <div style={{display: 'flex', flexDirection: 'column', marginLeft: '10px', marginRight: '10px'}}>
+            <h3>Dormitory Affairs Chair</h3>
+            <ul>
+              <li>Kirby Kimball</li>
+            </ul>
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column', marginLeft: '10px', marginRight: '10px'}}>
+            <h3>Class President '23</h3>
+            <ul>
+              <li>Sam Bogen</li>
+              <li>Sophie Dvorkin</li>
+              <li>Amari Huang</li>
+            </ul>
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column', marginLeft: '10px', marginRight: '10px'}}>
+            <h3>Class President '24</h3>
+            <ul>
+              <li>Cristian Pang</li>
+              <li>Ian Blaime</li>
+            </ul>
+            </div>
+
+            <div style={{display: 'flex', flexDirection: 'column', marginLeft: '10px', marginRight: '10px'}}>
+            <h3>Class President '25</h3>
+            <ul>
+              <li>Giovanni Pierre</li>
+              <li>Bryan Soh</li>
+            </ul>
+            </div>
+
+          </div>
+
+          </div>
+
           <h1>President</h1>
           <Candidate candidateInfo={president_candidate_data}/>
           <h1>Executive Vice President</h1>
